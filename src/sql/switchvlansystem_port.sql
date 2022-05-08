0351f65d13d4826f8f2ea67ecb05b2b93b922f8c
B-Werth <bastian619werth@gmail.com>
2022-05-08 23:34:16 +0200

    sql

---
+++ src/sql/switchvlansystem_port.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: switchvlansystem
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `port`
+--
+
+DROP TABLE IF EXISTS `port`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `port` (
+  `PortID` int NOT NULL AUTO_INCREMENT,
+  `PortNr` int NOT NULL,
+  `VlanID` int NOT NULL,
+  `SwitchID` int NOT NULL,
+  PRIMARY KEY (`PortID`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `port`
+--
+
+LOCK TABLES `port` WRITE;
+/*!40000 ALTER TABLE `port` DISABLE KEYS */;
+INSERT INTO `port` VALUES (1,1,1,0),(2,2,2,0),(3,3,3,0),(4,4,2,0),(5,5,3,0),(6,6,3,0),(7,7,2,0),(8,8,1,0),(9,9,3,0),(10,10,2,0),(11,11,2,0),(12,12,1,0),(13,13,3,0),(14,14,3,0),(15,15,3,0),(16,16,2,0),(17,17,1,0),(18,18,1,0),(19,19,2,0),(20,20,3,0),(21,21,3,0),(22,22,2,0),(23,23,2,0),(24,24,2,0);
+/*!40000 ALTER TABLE `port` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-08 23:31:54
